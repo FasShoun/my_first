a64cd9cad3c6fda64c221b6fdb006fa908cb875f
unknown <taslima622945@gmail.com>
2022-11-11 20:55:20 +0600

    first commit

---
+++ coustom.js
@@ -0,0 +1,13 @@
+"use strict";
+const head = document.getElementById("h1");
+setInterval(()=>{
+    head.style.color = "green";
+    head.style.backgroundColor = "purple";
+    head.style.fontSize = "3.5rem";
+},2000);
+setInterval(()=>{
+    head.style.color = "purple";
+    head.style.backgroundColor = "green";
+    head.style.fontSize = "2rem";
+
+},4000)
